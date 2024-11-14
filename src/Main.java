ed3c7a8ecf480cc46822fca027dba4a2a168b289
Дмитрий <prostodimas1337@gmail.com>
2024-11-14 03:40:22 +0300

    Ещё раз доделал

--- src/Main.java
+++ src/Main.java
@@ -34,11 +34,6 @@ public class Main {
                 }
             }
         }
-        boolean isSetLast = false;         //переменная для знания, было ли изменено последнее число на 1(если последнее число не 1, не получится)
-        if(list.getLast() != 1) {
-            list.set(list.size() - 1, 1);
-            isSetLast = true;
-        }
         int sameIndexAndNumCount = 0;           //счётчик числе равным индексу
         int sameIndexAndNum = 0;                //сохранение индекса, с которым совпадает его число(понадобится в конце)
         for(int i = 0; i < list.size(); i++) {
@@ -59,7 +54,7 @@ public class Main {
                 if(a == list.get(j)) {
                     sameNumber.add(a);                   //добавляем повторяющееся число
                     sameNumCount++;
-                    if(sameNumber.size() > 1 || isSetLast || sameNumCount > 1) {    //если повторяющихся чисел больше 1, или нашло повторяющееся, но последний элемент был изменён, то не получится
+                    if(sameNumber.size() > 1 || sameNumCount > 1) {    //если повторяющихся чисел больше 1, или нашло повторяющееся, но последний элемент был изменён, то не получится
                         System.out.println("-1 -1");
                         return;
                     }
@@ -72,18 +67,7 @@ public class Main {
                 return;
             }
         }
-        if(sameNumber.size() == 1) {         //если повторяющееся число
-            int missingNum = findMissingNumber(list);         //находим элемент, которого нет в последовательности (все элементы должны быть уникальны)
-            int indexToSet;                                   //переменная для сохранения индекса, число которого нужно изменить
-            if(list.indexOf(sameNumber.getFirst()) == missingNum){               //если индекс, где впервые встречается повторяющееся число, совпадает с пропущенным числом
-                indexToSet = list.lastIndexOf(sameNumber.getFirst()) + 1;           //ставим пропущенное число на то место, где повторяющееся встречается второй раз(запоминаем индекс)
-                System.out.println(indexToSet+" "+missingNum);
-            } else {
-                indexToSet = list.indexOf(sameNumber.getFirst()) + 1;         //иначе, ставим на место, где впервые встречается(запоминаем индекс)
-                System.out.println(indexToSet+" "+missingNum);
-            }
-            return;
-        } else if(sameIndexAndNumCount == 1) {         //если число совпадает со своим индексом
+        if(sameIndexAndNumCount == 1) {         //если число совпадает со своим индексом
             int missingNum = findMissingNumber(list);          //находим пропущенное число
             if(missingNum == 0) {                            //если нет пропущенных, то не получится
                 System.out.println("-1 -1");
@@ -91,10 +75,17 @@ public class Main {
             }
             int indexToSet = sameIndexAndNum + 1;          //присваиваем индекс с которым совпадает число
             System.out.println(indexToSet+" "+missingNum);
-            return;
-        }
-        if(isSetLast) {
-            System.out.println(n+" "+1);   //если нужно было только заменить последнее значение на 1, выводим
+
+        } else if(sameNumber.size() == 1) {         //если повторяющееся число
+            int missingNum = findMissingNumber(list);         //находим элемент, которого нет в последовательности (все элементы должны быть уникальны)
+            int indexToSet;                                   //переменная для сохранения индекса, число которого нужно изменить
+            if(list.indexOf(sameNumber.getFirst()) + 1 == missingNum){               //если индекс, где впервые встречается повторяющееся число, совпадает с пропущенным числом
+                indexToSet = list.lastIndexOf(sameNumber.getFirst()) + 1;           //ставим пропущенное число на то место, где повторяющееся встречается второй раз(запоминаем индекс)
+                System.out.println(indexToSet+" "+missingNum);
+            } else {
+                indexToSet = list.indexOf(sameNumber.getFirst()) + 1;         //иначе, ставим на место, где впервые встречается(запоминаем индекс)
+                System.out.println(indexToSet+" "+missingNum);
+            }
         }
     }
 
@@ -102,9 +93,9 @@ public class Main {
         HashSet<Integer> set = new HashSet<>(list);              //хэш сет для того, чтобы убрать не уникальные элементы
         ArrayList<Integer> sortedList = new ArrayList<>(set);         //лист с уникальными элементами для сортировки
         int missingNum = 0;
-        for(int i = 1; i <= sortedList.size(); i++) {
-            if(!sortedList.contains(i)){       //если число не совпало со своим индексом, то пропущенный элемент индекс
-                missingNum = i;
+        for(int i = 0; i < list.size(); i++) {
+            if(!sortedList.contains(i+1)){       //если число не совпало со своим индексом, то пропущенный элемент индекс
+                missingNum = i+1;
                 break;
             }
         }
